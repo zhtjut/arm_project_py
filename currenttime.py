453d09f693f63aa20570f565fa40a73044f34b5d
cbwuyanzu <277850402@qq.com>
2016-06-16 19:38:35 +0800

    month

--- currenttime.py
+++ currenttime.py
@@ -2,4 +2,5 @@ import time
 
 
 def get_current_time():
-    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+    return time.strftime("%d/%h/%Y %H:%M:%S", time.localtime(time.time()))
+
